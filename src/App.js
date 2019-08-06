a90be72ceec5ee3078ebb9b4ebec1efa1f09a8cb
Michael Szekley <michael.szekley@Michaels-MacBook-Pro-2.local>
2019-08-06 13:29:52 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+
+    }
+  }
+
+  componentDidMount() {
+    this.getMetaCopy()
+  }
+
+  getMetaCopy = async() => {
+    console.log('Hello World')
+  }
+
+  render() {
+    return (
+      <div className="jumbotron welcome">
+        <h1 className="display-4">Welcome</h1>
+        <p className="lead">This is Project Arthur</p>
+        <hr className="my-4" />
+        <p>A tool is designed to make app approvals easier by consolidating all the data needed.</p>
+      </div>
+    );
+  }
 }
 
 export default App;
